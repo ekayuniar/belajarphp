e39852f4c79e2eaffc3d630f5145c9c4dae4451f
eka yuniar <sayayuniar@gmail.com>
2023-03-02 19:54:43 +0700

    task 3 while

--- loop.php
+++ loop.php
@@ -1,6 +1,16 @@
 <?php
 // perulangan For
-
+echo "<H3>Latihan For</H3><br>";
 for ($i = 0; $i < 4; $i++) {
     echo "Hai saya urutan ke-$i <br>";
 };
+
+echo "<br><H3>Latihan While</H3>";
+
+// perulangan while
+$a = 0; // initial statement
+while ($a < 5) // Kondisi yang bernilai trur
+{
+    echo "Urutan ke $a <br>"; // eksekusi
+    $a++; // increment
+}
