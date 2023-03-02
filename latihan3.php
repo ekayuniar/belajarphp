da1f197fb68e2873e50c8ee3c5b65846c9c36e98
eka yuniar <sayayuniar@gmail.com>
2023-03-02 20:25:58 +0700

    Task Nilai

--- latihan3.php
+++ latihan3.php
@@ -1,9 +1,15 @@
 <?php
-$x = 9;
-if ($x < 10) {
-    echo "Benar";
-} elseif ($x == 10) {
-    echo "Binggo";
+$x = 61;
+if ($x < 50) {
+    echo "Nilai E";
+} elseif ($x >= 50 && $x < 60) {
+    echo "Nilai D";
+} elseif ($x >= 60 && $x < 70) {
+    echo "Nilai C";
+} elseif ($x >= 70 && $x < 80) {
+    echo "Nilai B";
+} elseif ($x >= 80 && $x < 90) {
+    echo "Nilai A";
 } else {
-    echo "Salah";
+    echo "Nilai A+";
 }
