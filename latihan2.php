24698236ed307dcf5af9dda88f646680afebe886
eka yuniar <sayayuniar@gmail.com>
2023-03-02 19:47:15 +0700

    belajar looping

--- latihan2.php
+++ latihan2.php
@@ -1,6 +1,8 @@
 <?php
-// concat
-$nama_depan = "Eka";
-$nama_belakang = "Yuniar";
-
-echo $nama_depan . " " . $nama_belakang;
+//belajar logika
+var_dump(1 < 5);
+echo "<br>";
+var_dump(1 === "1");
+echo "<br>";
+$x = 11;
+var_dump($x < 20 && $x % 2 == 0);
---
+++ loop.php
@@ -0,0 +1,6 @@
+<?php
+// perulangan For
+
+for ($i = 0; $i < 4; $i++) {
+    echo "Hai saya urutan ke-$i <br>";
+};
