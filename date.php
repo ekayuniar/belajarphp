ba77bddeff1efbbe956c031d94813380e07289cd
eka yuniar <sayayuniar@gmail.com>
2023-03-08 20:17:23 +0700

    Task 7

--- date.php
+++ date.php
@@ -1,2 +1,21 @@
-<?php
-echo date("l", mktime(2, 10, 5, 3, 2, 2023));
+<?php 
+function salam($waktu, $nama)
+{
+    return "Selamat $waktu $nama";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Function</title>
+</head>
+
+<body>
+    <h1><?= salam("Malam", "Eka");  ?></h1>
+</body>
+
+</html>
